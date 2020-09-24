2f5894514119e4eabf0c0190c8e253556bb04c12
Mr.Gao <4599870@qq.com>
2020-09-24 16:41:22 +0800

    完善 创建任务、暂停任务、开始任务、删除任务、简单添加任务等

--- onethingcloud/__init__.py
+++ onethingcloud/__init__.py
@@ -55,7 +55,7 @@ class OneThingCloudClient:
         self.__init_turn_server_info()
 
         # 启动定时器 获取任务列信息
-        self.__scheduler.add_job(self.__init_task_list, 'interval', seconds=10, id='JOB_INIT_TASK_LIST')
+        self.__scheduler.add_job(self.__init_task_info, 'interval', seconds=10, id='JOB_INIT_TASK_LIST')
 
         # 启动定时器
         self.__scheduler.start()
@@ -65,6 +65,12 @@ class OneThingCloudClient:
         self.__scheduler.shutdown(wait=False)
 
     def __send_json(self, url, json_data=''):
+        """
+        发送json数据
+        :param url:
+        :param json_data:
+        :return:
+        """
         self.__request_handler.headers = {
             'User-Agent': "Mozilla/5.0",
             "cache-control": "no-cache",
@@ -77,6 +83,13 @@ class OneThingCloudClient:
             raise Exception('ERR:%s,MSG:%s' % (result.status_code, 'request send failed.'))
 
     def __send(self, url, method, params=''):
+        """
+        发送请求
+        :param url:
+        :param method:
+        :param params:
+        :return:
+        """
         self.__request_handler.headers = {
             'User-Agent': "Mozilla/5.0",
             "cache-control": "no-cache"
@@ -91,20 +104,32 @@ class OneThingCloudClient:
             raise Exception('ERR:%s,MSG:%s' % (result.status_code, 'request send failed.'))
 
     def __init(self):
+        """
+        初始化
+        :return:
+        """
         params = [{"act": config.ACT, "app_version": config.APP_VERSION, "os_version": config.OS_VERSION,
                    "peerid": config.PEER_ID, "tabid": config.TAB_ID, "time": str(int(time.time()))}]
         str_params = json.dumps(params)
         res = self.__send(config.URL_INIT, self.__method_post, str_params)
+        logging.debug('init\nparams:%s\nresult:%s' % (params, res))
         if res['code'] == 0:
             logging.info('onethingcloud client init ok.')
         else:
             raise Exception('ERR:%s,MSG:%s' % ('1', 'client init failed.'))
 
     def __login(self, username, password):
+        """
+        登陆
+        :param username:
+        :param password:
+        :return:
+        """
         pwd = utils.get_pwd(password)
         params = utils.get_body(account_type=config.ACCOUNT_TYPE, peerid=config.PEER_ID, phone=username,
                                 phone_area=config.PHONE_AREA, product_id=config.PRODUCT_ID, pwd=pwd)
         res = self.__send(config.URL_LOGIN, self.__method_post, params)
+        logging.debug('login\nparams:%s\nresult:%s' % (params, res))
         i_ret = res['iRet']
         if i_ret == 0:
             self.__user_info = res['data']
@@ -112,19 +137,29 @@ class OneThingCloudClient:
             raise Exception('ERR:%s,MSG:%s' % (res['iRet'], res['sMsg']))
 
     def __check_session(self):
+        """
+        检查session
+        :return:
+        """
         params = utils.get_body(peerid=config.PEER_ID, product_id=config.PRODUCT_ID,
                                 protocol_version=config.PROTOCOL_VERSION, sessionid=self.__user_info['sessionid'],
                                 userid=self.__user_info['userid'])
         res = self.__send(config.URL_CHECK_SESSION, self.__method_post, params)
+        logging.debug('check_session\nparams:%s\nresult:%s' % (params, res))
         i_ret = res['iRet']
         if i_ret != 0:
             # 如果检查失败重新登陆下
             self.__login(self.__username, self.__password)
 
     def __init_peer_info(self):
+        """
+        获取设备信息
+        :return:
+        """
         params = utils.get_params(dict(appversion=config.APP_VERSION, ct='5', v='8'), self.__user_info['sessionid'],
                                   True)
         res = self.__send(config.URL_LIST_PEER + params, self.__method_get)
+        logging.debug('init_peer_info\nparams:%s\nresult:%s' % (params, res))
         rtn = res['rtn']
         if rtn == 0:
             devices = res['result'][1]['devices']
@@ -135,6 +170,10 @@ class OneThingCloudClient:
             raise Exception('ERR:%s,MSG:%s' % (rtn, res['msg']))
 
     def __init_turn_server_info(self):
+        """
+        获取内网穿透地址
+        :return:
+        """
         if not self.__device:
             logging.error("device info not init or device offline.")
             return None
@@ -144,6 +183,7 @@ class OneThingCloudClient:
                                   self.__user_info['sessionid'],
                                   True)
         res = self.__send(config.URL_GET_TURN_SERVER + params, self.__method_get)
+        logging.debug('init_turn_server_info1\nparams:%s\nresult:%s' % (params, res))
         rtn = res['rtn']
         if rtn == 0:
             self.__turn_server_1 = res['turn_server_addr']
@@ -151,18 +191,23 @@ class OneThingCloudClient:
             raise Exception('ERR:%s,MSG:%s' % (rtn, res['msg']))
 
         res2 = self.__send(config.URL_GET_TURN_SERVER + params, self.__method_get)
+        logging.debug('init_turn_server_info2\nparams:%s\nresult:%s' % (params, res))
         rtn2 = res2['rtn']
         if rtn2 == 0:
             self.__turn_server_2 = res2['turn_server_addr']
         else:
             raise Exception('ERR:%s,MSG:%s' % (rtn2, res2['msg']))
 
-    def __init_task_list(self):
+    def __init_task_info(self):
+        """
+        初始化任务信息
+        :return:
+        """
         pid = self.__device['peerid']
         params = utils.get_params_no_sign(dict(v='2', pid=pid, ct='31', ct_ver=config.APP_VERSION, pos='0',
                                                number='0', type='4', needUrl='0'))
-        logging.debug('init_task_list:%s' % params)
         res = self.__send(config.URL_CONTROL_REMOTE_LIST + params, self.__method_get)
+        logging.debug('init_task_info\nparams:%s\nresult:%s' % (params, res))
         rtn = res['rtn']
         if rtn == 0:
             self.__task_info['recycleNum'] = res['recycleNum']
@@ -188,16 +233,29 @@ class OneThingCloudClient:
         return self.__device
 
     def get_turn_server_info(self):
+        """
+        获取内网穿透服务信息
+        :return:
+        """
         return self.__turn_server_1, self.__turn_server_2
 
-    def get_task_list(self):
+    def get_task_info(self):
+        """
+        获取任务信息
+        :return:
+        """
         return self.__task_info
 
     def get_cloud_task_list(self):
+        """
+        获取云添加任务列表
+        :return:
+        """
         pid = self.__device['peerid']
         params = utils.get_body(v='2', pid=pid, ct='31', ct_ver=config.APP_VERSION, pos='0',
                                 number='0', type='4', needUrl='100')
         res = self.__send(config.URL_CONTROL_REMOTE_LIST, self.__method_post, params)
+        logging.debug('get_cloud_task_list\nparams:%s\nresult:%s' % (params, res))
         rtn = res['rtn']
         if rtn == 0:
             self.__task_info['tasks'] = res['tasks']
@@ -206,6 +264,15 @@ class OneThingCloudClient:
             raise Exception('ERR:%s,MSG:%s' % (res['rtn'], res['msg']))
 
     def del_cloud_task(self, task_id, task_state, task_type, delete_file=False, recycle_task=False):
+        """
+        删除任务
+        :param task_id:
+        :param task_state:
+        :param task_type:
+        :param delete_file:
+        :param recycle_task:
+        :return:
+        """
         pid = self.__device['peerid']
         tasks = task_id + '_' + task_state + '_' + task_type
 
@@ -213,6 +280,7 @@ class OneThingCloudClient:
             dict(pid=pid, ct='31', clientType='PC-onethingcloud', ct_ver=config.APP_VERSION, v='1',
                  tasks=tasks, deleteFile=str(delete_file).lower(), recycleTask=str(recycle_task).lower()))
         res = self.__send(config.URL_CONTROL_REMOTE_DEL + params, self.__method_get)
+        logging.debug('del_cloud_task\nparams:%s\nresult:%s' % (params, res))
         rtn = res['rtn']
         if rtn == 0:
             self.__task_info['tasks'] = []
@@ -230,13 +298,16 @@ class OneThingCloudClient:
         url_params = 'v=2&pid=' + pid + '&ct=31&ct_ver=' + config.APP_VERSION
         json_params = json.dumps({'url': url})
         res = self.__send_json(config.URL_CONTROL_REMOTE_URL_RESOLVE + url_params, json_params)
+        logging.debug('url_resolve\nparams:%s\nresult:%s' % (json_params, res))
         rtn = res['rtn']
         if rtn == 0:
+            if res['taskInfo']['name'] == '' or res['taskInfo']['size'] == '0':
+                raise Exception('ERR:-1,MSG:url resolve failed,please try again.')
             return res['taskInfo']
         else:
             raise Exception('ERR:%s,MSG:%s' % (res['rtn'], res['msg']))
 
-    def create_batch_task(self, url, file_size, name, task_type, bt_sub):
+    def create_batch_task(self, url, file_size, name, task_type, bt_sub=None):
         """
         创建下载任务
         :param url:
@@ -246,6 +317,8 @@ class OneThingCloudClient:
         :param bt_sub:
         :return:
         """
+        if bt_sub is None:
+            bt_sub = []
         pid = self.__device['peerid']
         url_params = 'v=2&pid=' + pid + '&ct=31&ct_ver=' + config.APP_VERSION
         json_params = json.dumps({
@@ -263,8 +336,69 @@ class OneThingCloudClient:
             }]
         })
         res = self.__send_json(config.URL_CONTROL_REMOTE_CREATE_BATCH_TASK + url_params, json_params)
+        logging.debug('create_batch_task\nparams:%s\nresult:%s' % (json_params, res))
         rtn = res['rtn']
         if rtn == 0:
             return res['tasks']
         else:
             raise Exception('ERR:%s,MSG:%s' % (res['rtn'], res['msg']))
+
+    def pause_task(self, task_id, task_state, task_type):
+        """
+        暂停任务
+        :param task_id:
+        :param task_state:
+        :param task_type:
+        :return:
+        """
+        pid = self.__device['peerid']
+        tasks = task_id + '_' + task_state + '_' + task_type
+
+        params = utils.get_params_no_sign(
+            dict(pid=pid, ct='31', clientType='PC-onecloud', ct_ver=config.APP_VERSION, v='1', tasks=tasks))
+        res = self.__send(config.URL_CONTROL_REMOTE_PAUSE + params, self.__method_get)
+        logging.debug('del_cloud_task\nparams:%s\nresult:%s' % (params, res))
+        rtn = res['rtn']
+        if rtn == 0:
+            return True
+        else:
+            raise Exception('ERR:%s,MSG:%s' % (res['rtn'], res['msg']))
+
+    def start_task(self, task_id, task_state, task_type):
+        """
+        开始任务
+        :param task_id:
+        :param task_state:
+        :param task_type:
+        :return:
+        """
+        pid = self.__device['peerid']
+        tasks = task_id + '_' + task_state + '_' + task_type
+
+        params = utils.get_params_no_sign(
+            dict(pid=pid, ct='31', clientType='PC-onecloud', ct_ver=config.APP_VERSION, v='1', tasks=tasks))
+        res = self.__send(config.URL_CONTROL_REMOTE_START + params, self.__method_get)
+        logging.debug('del_cloud_task\nparams:%s\nresult:%s' % (params, res))
+        rtn = res['rtn']
+        if rtn == 0:
+            return True
+        else:
+            raise Exception('ERR:%s,MSG:%s' % (res['rtn'], res['msg']))
+
+    def create_download_task(self, url):
+        """
+        创建下载任务(内部解析地址并创建下载任务)
+        :param url:
+        :return:
+        """
+        task_info = self.url_resolve(url)
+        task_url = task_info['url']
+        # 1 普通下载文件 ftp http 电驴 2 磁力链接 bt 迅雷链接
+        task_type = task_info['type']
+        task_name = task_info['name']
+        task_file_size = task_info['size']
+        bt_sub = []
+        if task_type == 2:
+            for sub in task_info['subList']:
+                bt_sub.append(sub['id'])
+        return self.create_batch_task(task_url, task_file_size, task_name, task_type, bt_sub)
